01205acdf6340c27f06ae4622767302ad8c1974f
SvetlanaBolshova <80790487+SvetlanaBolshova@users.noreply.github.com>
2021-08-27 00:06:28 +0300

    Add files via upload

---
+++ BolshovaSVdz3/App.java
@@ -0,0 +1,13 @@
+package org.BolshovaSVdz3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
